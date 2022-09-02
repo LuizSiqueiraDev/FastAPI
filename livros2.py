c51f8695db4f26231dc4cd6e8266e1a0a9a353af
LuizSiqueiraDev <luiz.siqueira1234@gmail.com>
2022-09-02 14:12:08 -0300

    add field na classe Livro

--- livros2.py
+++ livros2.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from uuid import UUID
 
 app = FastAPI()
@@ -7,10 +7,10 @@ app = FastAPI()
 
 class Livro(BaseModel):
     id: UUID
-    titulo: str
-    autor: str
-    descricao: str
-    avaliacao: int
+    titulo: str = Field(min_length=1)
+    autor: str = Field(min_length=1, max_length=100)
+    descricao: str|None = Field(title="Descrição do livro", min_length=1, max_length=100)
+    avaliacao: int = Field(gt=-1, lt=101)
 
 
 LIVROS = []
@@ -22,6 +22,6 @@ async def mostrar_livros():
 
 
 @app.post("/")
-async def criar_livro(livro: Livro):
+async def adicionar_livro(livro: Livro):
     LIVROS.append(livro)
     return livro
