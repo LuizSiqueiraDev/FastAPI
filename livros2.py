bd16da1e2c74de6accc75b65495ef907a19a4c70
LuizSiqueiraDev <luiz.siqueira1234@gmail.com>
2022-09-02 14:28:20 -0300

    add função cadastrar_livros_sem_api

--- livros2.py
+++ livros2.py
@@ -18,6 +18,8 @@ LIVROS = []
 
 @app.get("/")
 async def mostrar_livros():
+    if len(LIVROS) < 1:
+        cadastrar_livros_sem_api()
     return LIVROS
 
 
@@ -25,3 +27,46 @@ async def mostrar_livros():
 async def adicionar_livro(livro: Livro):
     LIVROS.append(livro)
     return livro
+
+
+def cadastrar_livros_sem_api():
+    livro_1 = Livro(
+        id="78187ea0-6730-4e4b-a5cc-aad1622c7e01", 
+        titulo="Título Um", 
+        autor="Autor Um", 
+        descricao="Descrição Um", 
+        avaliacao=10
+        )
+    livro_2 = Livro(
+        id="78187ea0-6730-4e4b-a5cc-aad1622c7e02", 
+        titulo="Título Dois", 
+        autor="Autor Dois", 
+        descricao="Descrição Dois", 
+        avaliacao=20
+        )
+    livro_3 = Livro(
+        id="78187ea0-6730-4e4b-a5cc-aad1622c7e03", 
+        titulo="Título Três", 
+        autor="Autor Três", 
+        descricao="Descrição Três", 
+        avaliacao=30
+        )
+    livro_4 = Livro(
+        id="78187ea0-6730-4e4b-a5cc-aad1622c7e04", 
+        titulo="Título Quatro", 
+        autor="Autor Quatro", 
+        descricao="Descrição Quatro", 
+        avaliacao=40
+        )
+    livro_5 = Livro(
+        id="78187ea0-6730-4e4b-a5cc-aad1622c7e05", 
+        titulo="Título Cinco", 
+        autor="Autor Cinco", 
+        descricao="Descrição Cinco", 
+        avaliacao=50
+        )
+    LIVROS.append(livro_1)
+    LIVROS.append(livro_2)
+    LIVROS.append(livro_3)
+    LIVROS.append(livro_4)
+    LIVROS.append(livro_5)
