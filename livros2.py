d6d83afacbbf8e0ac71f52ec99e58538ca709267
LuizSiqueiraDev <luiz.siqueira1234@gmail.com>
2022-09-02 13:00:33 -0300

    add classe Livro

--- livros2.py
+++ livros2.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from uuid import UUID
 
 app = FastAPI()
+
+
+class Livro(BaseModel):
+    id: UUID
+    titulo: str
+    autor: str
+    descricao: str
+    avaliacao: int
+
 
 LIVROS = []
 
