5f9437b5d09a970f415095112bff74214d17dfb4
LuizSiqueiraDev <luiz.siqueira1234@gmail.com>
2022-09-04 16:37:24 -0300

    models adicionado

---
+++ ObrasAPP/models.py
@@ -0,0 +1,12 @@
+from email.policy import default
+from sqlalchemy import Boolean, Column, Integer, String
+from database import Base
+
+
+class Obras(Base):
+    __tablename__ = "obras"
+    id = Column(Integer, primary_key=True, index=True)
+    titulo = Column(String)
+    descricao = Column(String)
+    prioridade = Column(Integer)
+    lido = Column(Boolean, default=False)
