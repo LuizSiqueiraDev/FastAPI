8a5d5c53ae662a0aee6d97d16ec869949d41b7f2
LuizSiqueiraDev <luiz.siqueira1234@gmail.com>
2022-09-06 10:53:36 -0300

    adicionado função obter_db e mostrar_obras

--- ObrasAPP/main.py
+++ ObrasAPP/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
-from database import engine
+from fastapi import FastAPI, Depends
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 import models
 
 app = FastAPI()
@@ -7,6 +8,14 @@ app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
 
+def obter_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
 @app.get("/")
-async def criar_database():
-    return {"Database": "Criada"}
+async def mostrar_obras(db: Session = Depends(obter_db)):
+    return db.query(models.Obras).all()
