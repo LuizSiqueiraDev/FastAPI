008a6ef161786e0b157e2de8930ab0847d8a7c11
LuizSiqueiraDev <luiz.siqueira1234@gmail.com>
2022-09-06 18:25:25 -0300

    adicionado classe Usuarios()

--- ObrasAPP/models.py
+++ ObrasAPP/models.py
@@ -1,6 +1,19 @@
-from email.policy import default
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
+
+
+class Usuarios(Base):
+    __tablename__ = 'usuarios'
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    apelido = Column(String, unique=True, index=True)
+    nome = Column(String)
+    sobrenome = Column(String)
+    senha_hashed = Column(String)
+    ativo =Column(Boolean, default=True)
+
+    livros = relationship("Livros", back_populates="dono")
 
 
 class Livros(Base):
@@ -11,3 +24,6 @@ class Livros(Base):
     descricao = Column(String)
     prioridade = Column(Integer)
     lido = Column(Boolean, default=False)
+    dono_id = Column(Integer, ForeignKey("usuarios.id"))
+
+    dono = relationship("Usuarios", back_populates="livros")
