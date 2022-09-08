6242f47986d0c274ea2ce8f28445ee152a4db705
LuizSiqueiraDev <luiz.siqueira1234@gmail.com>
2022-09-08 15:08:52 -0300

    import bcrypt

--- ObrasAPP/autorizacao.py
+++ ObrasAPP/autorizacao.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from passlib.context import CryptContext
 import models
 
 app = FastAPI()
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 class CriarUsuario(BaseModel):
@@ -13,6 +16,10 @@ class CriarUsuario(BaseModel):
     senha: str
 
 
+def obter_senha_hash(senha):
+    return bcrypt_context.hash(senha)
+
+
 @app.post("/criar/usuario")
 async def criar_usuario(criar_usuario: CriarUsuario):
     modelo = models.Usuarios()
@@ -20,7 +27,10 @@ async def criar_usuario(criar_usuario: CriarUsuario):
     modelo.apelido = criar_usuario.apelido
     modelo.nome = criar_usuario.nome
     modelo.sobrenome = criar_usuario.sobrenome
-    modelo.senha_hashed = criar_usuario.senha
+
+    senha_hash = obter_senha_hash(criar_usuario.senha)
+    modelo.senha_hashed = senha_hash
+    
     modelo.ativo = True
 
     return modelo
